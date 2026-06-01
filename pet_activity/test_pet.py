59d58c57147749d91fac65fb8fc6291fabb131f3
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 01:08:05 +0800

    creation of storing the data using our mutator methods...

--- pet_activity/test_pet.py
+++ pet_activity/test_pet.py
@@ -9,3 +9,6 @@ if __name__ == "__main__":
     pet_type = input("Enter your pet's animal type (e.g., Dog, Cat, Bird): ")
     pet_age = int(input("Enter your pet's age: "))
 
+    my_pet.set_name(pet_name)
+    my_pet.set_animal_type(pet_type)
+    my_pet.set_age(pet_age)
