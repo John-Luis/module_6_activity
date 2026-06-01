90beb0483cb850f04cad93d8fd5e662a5f23d899
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 00:51:31 +0800

    starting the creation of the getter and setter method. We started with the name. By implementing this methods, we safely modify and access the encapsulated private fields.

--- pet_class.py
+++ pet_class.py
@@ -1,8 +1,16 @@
 
 # pet_class.py
-class Pet:
+class pet:
     def __init__(self):
         # Initialize hidden attributes using double underscores as private members
         self.__name = ""
         self.__animal_type = ""
         self.__age = 0
+
+    # Mutator methods (Setters)
+    def set_name(self, name):
+        self.__name = name
+
+    # Accessor methods (Getters)
+    def get_name(self):
+        return self.__name
