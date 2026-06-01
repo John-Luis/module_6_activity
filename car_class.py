c94a8acad0cda9607e42b6b522b6a8e58e83d49f
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 00:30:11 +0800

    created the acceleration method in our class car. Where it adds 5km/h to current speed if it was called.

--- car_class.py
+++ car_class.py
@@ -5,3 +5,7 @@ class car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0  # Default speed starts at 0
+
+        # Accelerate method adds 5 km/h to current speed
+        def accelerate(self):
+            self.__speed += 5
