6634988e95f563a73020a8ad9429d0b4a1613fe0
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 00:53:35 +0800

    Added fully the method for our getter and setter... (particularl the age and animal type attributes)

--- pet_class.py
+++ pet_class.py
@@ -14,3 +14,19 @@ class pet:
     # Accessor methods (Getters)
     def get_name(self):
         return self.__name
+
+    # Mutator methods (Setters)
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
+    # Accessor methods (Getters)
+    def get_animal_type(self):
+        return self.__animal_type
+
+    # Mutator methods (Setters)
+    def set_age(self, age):
+        self.__age = age
+
+    # Accessor methods (Getters)
+    def get_age(self):
+        return self.__age
