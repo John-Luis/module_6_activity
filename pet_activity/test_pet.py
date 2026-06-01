dbeff17602d7270916e5f4618b8f08f74d561d8a
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 01:07:13 +0800

    added the input function for the user prompts that will be putted to the attributes.

--- pet_activity/test_pet.py
+++ pet_activity/test_pet.py
@@ -1,6 +1,11 @@
 # test_pet.py
-from pet_class import Pet
+from pet_class import pet
 
 if __name__ == "__main__":
     # Create an object of the Pet class
-    my_pet = Pet()
+    my_pet = pet()
+
+    pet_name = input("Enter your pet's name: ")
+    pet_type = input("Enter your pet's animal type (e.g., Dog, Cat, Bird): ")
+    pet_age = int(input("Enter your pet's age: "))
+
