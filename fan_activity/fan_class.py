d4e2986ee2c70e8325b3f558546b54b20b58ca86
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 01:09:35 +0800

    updated a bug where indentation is wrong in the py file.

--- fan_activity/fan_class.py
+++ fan_activity/fan_class.py
@@ -1,42 +1,41 @@
 
 
 class fan:
-    class fan:
-        # Class constants
-        SLOW = 1
-        MEDIUM = 2
-        FAST = 3
+    # Class constants
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
 
-        # Constructor with required default values
-        def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
-            # Private data fields (Enforced with double underscores)
-            self.__speed = speed
-            self.__radius = radius
-            self.__color = color
-            self.__on = on
+    # Constructor with required default values
+    def __init__(self, speed=SLOW, radius=5, color="blue", on=False):
+        # Private data fields (Enforced with double underscores)
+        self.__speed = speed
+        self.__radius = radius
+        self.__color = color
+        self.__on = on
 
-        # --- ACCESSOR METHODS (Getters) ---
-        def get_speed(self):
-            return self.__speed
+    # --- ACCESSOR METHODS (Getters) ---
+    def get_speed(self):
+        return self.__speed
 
-        def get_radius(self):
-            return self.__radius
+    def get_radius(self):
+        return self.__radius
 
-        def get_color(self):
-            return self.__color
+    def get_color(self):
+        return self.__color
 
-        def get_on(self):
-            return self.__on
+    def get_on(self):
+        return self.__on
 
-        # --- MUTATOR METHODS (Setters) ---
-        def set_speed(self, speed):
-            self.__speed = speed
+    # --- MUTATOR METHODS (Setters) ---
+    def set_speed(self, speed):
+        self.__speed = speed
 
-        def set_radius(self, radius):
-            self.__radius = radius
+    def set_radius(self, radius):
+        self.__radius = radius
 
-        def set_color(self, color):
-            self.__color = color
+    def set_color(self, color):
+        self.__color = color
 
-        def set_on(self, on):
-            self.__on = on
+    def set_on(self, on):
+        self.__on = on
