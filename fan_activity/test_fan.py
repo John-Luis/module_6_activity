f8fad1914c91fba086bff2423c18b3414e596082
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-06-02 00:10:42 +0800

    renamed the py folder into snake case.. also other variables.

--- fan_activity/test_fan.py
+++ fan_activity/test_fan.py
@@ -1,6 +1,6 @@
 # We import the Fan class from our fan.py file
-from fan import Fan
+from fan_class import fan
 
 if __name__ == "__main__":
     # Object 1: maximum speed (FAST), radius 10, color yellow, turned on
-    fan_1 = Fan(speed=Fan.FAST, radius=10, color="yellow", on=True)
+    fan_1 = fan(speed=fan.FAST, radius=10, color="yellow", on=True)
